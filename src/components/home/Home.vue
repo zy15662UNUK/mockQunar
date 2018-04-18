c38bc4bc95536ad9d3278ed6dfba732a85d4a4e4
Jiahao LUO <2679229323@qq.com>
2018-04-18 10:22:03 +0100

    init finish

---
+++ src/components/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
