3dee5340310d1d04b13e19f235349365d9eedfc0
Jiahao LUO <2679229323@qq.com>
2018-04-24 15:34:34 +0100

    finish city router

---
+++ src/components/city/City.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="City">
+    <CityHeader></CityHeader>
+  </div>
+</template>
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: "City",
+  data: function(){
+    return {
+
+    };
+  },
+  components: {
+    CityHeader: Header
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/components/city/components/Header.vue
+++ src/components/city/components/Header.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="header">
     <div class="header-left float-left">
-      <i class="fas fa-chevron-left"></i>
+      <router-link :to="{ name: 'Home'}"><i class="fas fa-chevron-left"></i></router-link>
     </div>
-    <div class="header-input">
+    <!-- <div class="header-input">
       <span class="iconfont">&#xe632;</span> 输入城市/景点/游玩主题
+    </div> -->
+    <div class="header-title">
+      城市选择
     </div>
     <div class="header-right float-right">
-       <router-link :to="{ name: 'City'}">{{this.city}} </router-link><i class="fas fa-sort-down"></i>
+       <!-- <router-link :to="{ name: 'City'}">{{this.city}} </router-link><i class="fas fa-sort-down"></i> -->
     </div>
   </div>
 </template>
@@ -34,12 +37,21 @@ export default {
     line-height: .86rem
     background-color: $bgColor;
     color: white
-
+    text-align: center
     .header-left
       width: 0.64rem
       text-align: center
 
-
+    .header-title
+      flex: 1;
+      font-size: 0.35rem
+      color: #fffff
+      margin-top: 0.12rem;
+      height: 0.64rem; /* 设置上边距+高度避免设置上下边距导致外部边框撑大/ */
+      line-height: 0.64rem;/* 使文字上下居中，和上一条合作避免下部触底 */
+      margin-left: 0.2rem;
+      border-radius: 0.1rem;
+      padding-left: 0.2rem;
     .header-input
       flex: 1;
       color: #ccc
